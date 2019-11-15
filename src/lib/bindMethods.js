3e3cb48bdbc8381b226ac4f0a0f7efee640b1bdd
David Fava <dfava80@gmail.com>
2019-11-15 13:11:24 +0000

    Add position methods

--- src/lib/bindMethods.js
+++ src/lib/bindMethods.js
@@ -3,6 +3,9 @@ const { Subject } = require('rxjs');
 const addOrder = require('../utils/orders/addOrder');
 const deleteOrder = require('../utils/orders/deleteOrder');
 const updateOrder = require('../utils/orders/updateOrder');
+const addPosition = require('../utils/positions/addPosition');
+const deletePosition = require('../utils/positions/deletePosition');
+const updatePosition = require('../utils/positions/updatePosition');
 const db = require('./db');
 
 module.exports = tvChart => {
@@ -16,9 +19,15 @@ module.exports = tvChart => {
 
   tvUtil.interactions$ = onInteracton$;
 
+  // Orders
   tvUtil.order.add = compose(addOrder);
   tvUtil.order.delete = compose(deleteOrder);
   tvUtil.order.update = compose(updateOrder);
+
+  // Positions
+  tvUtil.position.add = compose(addPosition);
+  tvUtil.position.delete = compose(deletePosition);
+  tvUtil.position.update = compose(updatePosition);
 
   return tvUtil;
 };
