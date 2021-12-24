1cd568f9d70751011f7e18f5fa1497579d89e158
David Fava <dfava80@gmail.com>
2021-12-24 21:24:15 +0100

    fix: correct issue with TvChart type (#23)

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+// import { babel } from '@rollup/plugin-babel';
+
+// this override is needed because Module format cjs does not support top-level await
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
+const globals = {
+  ...packageJson.devDependencies,
+};
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd',
+    name: 'tv-lines',
+    sourcemap: true,
+    exports: 'named',
+  },
+  plugins: [
+    peerDepsExternal(),
+    commonjs({ transformMixedEsModules: true }),
+    nodePolyfills(),
+    resolve({ browser: true, preferBuiltins: true }),
+    typescript({
+      objectHashIgnoreUnknownHack: true,
+      abortOnError: false,
+    }),
+  ],
+  external: Object.keys(globals),
+};
